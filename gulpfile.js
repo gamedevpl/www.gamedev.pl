d5c71b6bd64792f259a26775a1df9120015a31cc
Grzegorz Tanczyk <fb@tanczyk.pl>
2015-08-13 13:15:19 +0200

    combine all sources into one json

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var concat = require('gulp-concat');
 var download = require("gulp-download");
 var ext_replace = require('gulp-ext-replace');
 var gulp = require('gulp');
+var jsoncombine = require("gulp-jsoncombine");
 var mustache = require("gulp-mustache");
 var less = require('gulp-less');
 var path = require('path');
@@ -10,30 +11,54 @@ var rm = require('gulp-rm')
 var run = require('gulp-run');
 var serve = require('gulp-serve');
 
+var merge = require('merge-stream');
+
 gulp.task('clean', function() {
     gulp.src('dist/**/*')
-    	.pipe(rm());
+        .pipe(rm());
 });
 
-gulp.task('fetch', function() {
-	download("https://forum.gamedev.pl/latest.json")
-	  .pipe(rename("latest.json"))
-	  .pipe(gulp.dest('./dist'));
+gulp.task('fetch_topics', function() {
+    download("https://forum.gamedev.pl/latest.json")
+      .pipe(rename("topics.json"))
+      .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('fetch_highlights', function() {
+    gulp.src("highlights/*.json")
+        .pipe(jsoncombine("highlights.json", function(data) {
+            return new Buffer(JSON.stringify(data));
+         }))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('fetch_jobs', function() {
+
+});
+
+gulp.task('fetch', ['fetch_topics', 'fetch_highlights', 'fetch_jobs'], function() {
+    gulp.src(['dist/topics.json', 
+              'dist/jobs.json', 
+              'dist/highlights.json'])
+         .pipe(jsoncombine("combined.json", function(data) {
+             return new Buffer(JSON.stringify(data))
+          }))
+         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('render', function() {
-	gulp.src("app/templates/**/*.mustache")
-    	.pipe(mustache("./dist/latest.json"))
-    	.pipe(ext_replace('.html'))
-    	.pipe(gulp.dest("./dist/public"));	
+    gulp.src("app/templates/**/*.mustache")
+        .pipe(mustache("./dist/combined.json"))
+        .pipe(ext_replace('.html'))
+        .pipe(gulp.dest("./dist/public"));    
 })
 
 gulp.task('less', function() {
-	return gulp.src('app/less/**/*.less')
-	    .pipe(less({
-		    paths : [ path.join(__dirname, 'less', 'includes') ]
-	    }))
-	    .pipe(gulp.dest('./dist/public/css'));
+    return gulp.src('app/less/**/*.less')
+        .pipe(less({
+            paths : [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./dist/public/css'));
 });
 
 gulp.task('build', ['less', 'render']);
