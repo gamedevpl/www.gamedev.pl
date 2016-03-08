d7ee717cbc51e0521c6a2ede3ee1df213ae815ea
Travis-CI <gamedevpl@travis-ci.org>
2016-03-08 20:02:39 +0000

    Update 2016-03-08T20:02:38.593Z

--- js/init.js
+++ js/init.js
@@ -1,20 +1,14 @@
 head.js('http://code.jquery.com/jquery-1.11.1.min.js', function() {
+        head.js('/js/jquery.selectbox-0.2.min.js', function() {
+            $('select').selectbox();
+        });
 
-		if ($('select').length > 0) {
-
-			head.js('/js/jquery.selectbox-0.2.min.js', function() {
-				$('select').selectbox();
-			});
-		}
-		
-		if ($(".slider").size()>0) {
-			head.js('/js/jquery.royalslider.custom.min.js');
-		}
+        head.js('/js/jquery.royalslider.custom.min.js', function() {
+            head.js('/js/main.js');
+        });
 	})
-	.js('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js')
-	.js('/js/main.js');
-	//.js('/js/main.js')
-	
+	.js('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js');
+
 
 if (head.browser.ie && parseFloat(head.browser.version) < 9) {
     head.js('https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js')
