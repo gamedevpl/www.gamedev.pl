7c87c5ff22308ed819ec6e223a1269e3073c46ad
Grzegorz Tańczyk <gtanczyk@atlassian.com>
2023-05-07 21:08:51 +0200

    Revert "check if faulty commit on master triggers deployment"
    This reverts commit d0d7ec423cd3824f89eb5b0a7ed3e00e9704ff19.

--- website/src/App.test.tsx
+++ website/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders something", () => {
   render(<App />);
-  const linkElement = screen.getByText(/O Gamedevx.pl/i);
+  const linkElement = screen.getByText(/O Gamedev.pl/i);
   expect(linkElement).toBeInTheDocument();
 });
