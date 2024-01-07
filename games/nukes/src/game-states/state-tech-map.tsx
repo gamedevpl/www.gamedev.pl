1380f5dbc1eaf3f01ac945140b36af759378a633
Grzegorz Tańczyk <fb@tanczyk.pl>
2024-01-07 18:06:01 +0100

    tech nuke progress

--- games/nukes/src/game-states/state-tech-map.tsx
+++ games/nukes/src/game-states/state-tech-map.tsx
@@ -1,53 +1,103 @@
-import React from 'react';
-import { GameState, GameStateComponent } from './types';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { CameraShake, OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+import { GameState, GameStateComponent } from './types';
 
 const MAP_WIDTH_TILES = 100;
 const MAP_HEIGHT_TILES = 100;
 
-const TILE_SIZE = 64;
+const TILE_SIZE = 0.25;
 
 const TILES = Array.from({ length: MAP_WIDTH_TILES })
-  .map((v, x) => Array.from({ length: MAP_HEIGHT_TILES }).map((v, y) => ({ x, y })))
+  .map((v, x) => Array.from({ length: MAP_HEIGHT_TILES }).map((v, y) => ({ x: x * TILE_SIZE, y: y * TILE_SIZE })))
   .flat();
 
-const Intro: GameStateComponent = ({ setGameState }) => {
+const TechMapComponent: GameStateComponent = ({ setGameState }) => {
   return (
-    <Map>
-      {TILES.map((tile) => (
-        <Tile style={{ '--tile-x': tile.x, '--tile-y': tile.y } as React.CSSProperties}>
-          [{tile.x},{tile.y}]
-        </Tile>
-      ))}
-    </Map>
+    <MapContainer>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <MapTiles />
+        <OrbitControls makeDefault />
+        <CameraShake
+          maxYaw={0.1} // Max amount camera can yaw in either direction
+          maxPitch={0.1} // Max amount camera can pitch in either direction
+          maxRoll={0.1} // Max amount camera can roll in either direction
+          yawFrequency={0.1} // Frequency of the the yaw rotation
+          pitchFrequency={0.1} // Frequency of the pitch rotation
+          rollFrequency={0.1} // Frequency of the roll rotation
+          intensity={1} // initial intensity of the shake
+          decayRate={0.65} // if decay = true this is the rate at which intensity will reduce at />
+        />
+      </Canvas>
+    </MapContainer>
   );
 };
 
-const Map = styled.div`
+function MapTiles() {
+  const [positions] = useState(
+    () =>
+      new Float32Array(
+        TILES.map((tile, index) => {
+          const z = Math.cos((index / TILES.length) * Math.PI) * TILE_SIZE * 10;
+
+          return [
+            [tile.x, tile.y, z], // v0
+            [tile.x + TILE_SIZE, tile.y, z], // v1
+            [tile.x + TILE_SIZE, tile.y + TILE_SIZE, z], // v2
+
+            [tile.x + TILE_SIZE, tile.y + TILE_SIZE, z], // v3
+            [tile.x, tile.y + TILE_SIZE, z], // v4
+            [tile.x, tile.y, z], // v5
+          ];
+        })
+          .flat()
+          .flat(),
+      ),
+  );
+
+  new THREE.Color();
+
+  const [colors] = useState(
+    () =>
+      new Float32Array(
+        TILES.map((tile, index) => [
+          [0, 1, 0],
+          new THREE.Color(0xff0000).toArray(),
+          new THREE.Color(0xff0000).toArray(),
+          new THREE.Color(0xff0000).toArray(),
+          new THREE.Color(0xff0000).toArray(),
+          new THREE.Color(0xff0000).toArray(),
+        ])
+          .flat()
+          .flat(),
+      ),
+  );
+
+  return (
+    <mesh>
+      <bufferGeometry>
+        <bufferAttribute attach="attributes-position" count={positions.length / 3} array={positions} itemSize={3} />
+        <bufferAttribute attach="attributes-color" count={colors.length / 3} array={colors} itemSize={3} />
+      </bufferGeometry>
+      <meshBasicMaterial vertexColors />
+    </mesh>
+  );
+}
+
+const MapContainer = styled.div`
   position: absolute;
-  transform: rotate(-45deg) skew(15deg, 15deg);
-  transform-origin: center;
-  transform-style: preserve-3d;
-  left: 0px;
-`;
-
-const Tile = styled.div`
-  position: absolute;
-  transform: translate(calc(var(--tile-x) * ${TILE_SIZE}px), calc(var(--tile-y) * ${TILE_SIZE}px));
-
-  width: ${TILE_SIZE}px;
-  height: ${TILE_SIZE}px;
-  overflow: hidden;
-
-  background: rgb(calc(var(--tile-x) / ${MAP_WIDTH_TILES} * 255), calc(var(--tile-y) / ${MAP_HEIGHT_TILES} * 255), 0);
-
-  &:hover {
-    background: yellow;
-    cursor: pointer;
-  }
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
 `;
 
 export const GameStateTechMap: GameState = {
-  Component: Intro,
+  Component: TechMapComponent,
   path: '/tech-map',
 };
--- games/nukes/src/game-states/state-tech-nuke.tsx
+++ games/nukes/src/game-states/state-tech-nuke.tsx
@@ -1,10 +1,75 @@
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { GameState, GameStateComponent } from './types';
+import styled from 'styled-components';
+import { Canvas, useThree } from '@react-three/fiber';
+import * as THREE from 'three';
 
-const Intro: GameStateComponent = ({ setGameState }) => {
-  return <>map</>;
+const TechNukeComponent: GameStateComponent = ({ setGameState }) => {
+  const [time, setTime] = useState<number>(1);
+
+  return (
+    <Container>
+      <Controls>
+        <input
+          type="range"
+          id="volume"
+          name="volume"
+          min="0"
+          max="1"
+          step="any"
+          value={time}
+          onChange={(event) => setTime(Number(event.target.value))}
+        />
+        <br />
+        Time: {time.toFixed(3)}
+      </Controls>
+      <Canvas>
+        <Explosion time={time} />
+      </Canvas>
+    </Container>
+  );
 };
 
+function Explosion({ time }: { time: number }) {
+  const points = useMemo(() => {
+    const points = new THREE.EllipseCurve(0, 0, 1 * time, 1.15 * time, 0, 2 * Math.PI, false, 0).getPoints(50);
+    return new Float32Array(points.map((v) => [v.x, v.y, 0]).flat());
+  }, [time]);
+
+  const { invalidate } = useThree();
+  const ref = useRef<THREE.Mesh>(null);
+
+  useEffect(() => {
+    invalidate();
+    ref.current!.geometry.attributes.position.needsUpdate = true;
+  }, [time]);
+
+  return (
+    <mesh ref={ref}>
+      <bufferGeometry>
+        <bufferAttribute attach="attributes-position" count={points.length / 3} array={points} itemSize={3} />
+      </bufferGeometry>
+      {/* <bufferAttribute attach="attributes-position" count={points.length / 3} array={points} itemSize={3} /> */}
+      <meshBasicMaterial color="royalblue" />
+    </mesh>
+  );
+}
+
+const Container = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Controls = styled.div`
+  text-align: left;
+  background: #eee;
+  padding: 5px 10px;
+`;
+
 export const GameStateTechNuke: GameState = {
-  Component: Intro,
-  path: '/tech-map',
+  Component: TechNukeComponent,
+  path: '/tech-nuke',
 };
