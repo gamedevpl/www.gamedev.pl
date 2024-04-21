415b8d4638bb484354998dd76fd226220d82d888
Grzegorz Tańczyk <fb@tanczyk.pl>
2024-04-21 15:01:02 +0200

    cleanup

--- games/nukes/src/App.tsx
+++ games/nukes/src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import { GameState } from './game-states/types';
 import { GameStateIntro } from './game-states/state-intro';
-import { GameStateTechMap } from './game-states/state-tech-map';
-import { GameStateTechNuke } from './game-states/state-tech-nuke';
 import { GameStateTechWorld } from './game-states/state-tech-world';
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
       <BrowserRouter basename="/">
         <Routes>
           <Route path={GameStateIntro.path} element={<GameStateRoute state={GameStateIntro} />} />
-          <Route path={GameStateTechMap.path} element={<GameStateRoute state={GameStateTechMap} />} />
-          <Route path={GameStateTechNuke.path} element={<GameStateRoute state={GameStateTechNuke} />} />
           <Route path={GameStateTechWorld.path} element={<GameStateRoute state={GameStateTechWorld} />} />
         </Routes>
       </BrowserRouter>
