fcbd0a9852f3030854da6557ce42b53a2aedfcae
Grzegorz Tańczyk <fb@tanczyk.pl>
2024-07-24 01:40:55 +0200

    SPA routing fix

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react(), checker({ typescript: true })],
+  plugins: [
+    react(),
+    checker({ typescript: true }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "build/index.html",
+          dest: ".",
+          rename: "404.html",
+        },
+      ],
+    }),
+  ],
 });
