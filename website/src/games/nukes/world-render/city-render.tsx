2a2b3d57a4760ede70e6d8e3301c24252e80a5fb
Grzegorz Tańczyk <fb@tanczyk.pl>
2024-07-28 01:18:08 +0200

    city display depends on population

--- website/src/games/nukes/world-render/city-render.tsx
+++ website/src/games/nukes/world-render/city-render.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
 
 import { City } from '../world/world-state-types';
@@ -7,6 +7,11 @@ import { useObjectPointer } from '../controls/pointer';
 export function CityRender({ city }: { city: City }) {
   const [point, unpoint] = useObjectPointer();
 
+  const currentPopulation = city.populationHistogram[city.populationHistogram.length - 1].population;
+  const maxPopulation = Math.max(...city.populationHistogram.map((entry) => entry.population));
+  const size = Math.max(5, 10 * (currentPopulation / maxPopulation));
+
+  // @CODEGEN: When hovering over CityContainer, display a tooltip with with city name and current population
   return (
     <CityContainer
       onMouseEnter={() => point(city)}
@@ -15,6 +20,8 @@ export function CityRender({ city }: { city: City }) {
         {
           '--x': city.position.x,
           '--y': city.position.y,
+          '--size': size,
+          '--opacity': currentPopulation > 0 ? 1 : 0.3,
         } as React.CSSProperties
       }
     />
@@ -24,7 +31,8 @@ export function CityRender({ city }: { city: City }) {
 const CityContainer = styled.div`
   transform: translate(calc(var(--x) * 1px), calc(var(--y) * 1px)) translate(-50%, -50%);
   position: absolute;
-  width: 10px;
-  height: 10px;
+  width: calc(var(--size) * 1px);
+  height: calc(var(--size) * 1px);
+  opacity: var(--opacity);
   background: rgb(0, 0, 255);
 `;
