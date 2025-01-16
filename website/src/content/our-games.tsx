3a25f56e1a3b31deed99591123bea13874c26d65
Grzegorz Tańczyk <fb@tanczyk.pl>
2025-01-16 11:45:04 +0100

    add GitHub links to game boxes; enhance GameBox component with source code link for each game

--- website/src/content/our-games.tsx
+++ website/src/content/our-games.tsx
@@ -17,6 +17,7 @@ export default function OurGames() {
         description={t('Świąteczna platformówka')}
         highlight={xmasHighlight}
         href={'/games/xmas/'}
+        githubUrl="https://github.com/gamedevpl/www.gamedev.pl/tree/master/games/xmas"
         inProgress={false}
       />
       <GameBox
@@ -24,6 +25,7 @@ export default function OurGames() {
         description={t('Symulacja wojny nuklearnej')}
         highlight={nukesHighlight}
         href={'/games/nukes/'}
+        githubUrl="https://github.com/gamedevpl/www.gamedev.pl/tree/master/games/nukes"
         inProgress={true}
       />
       <GameBox
@@ -31,6 +33,7 @@ export default function OurGames() {
         description={t('Turowa gra logiczna')}
         highlight={monsterStepsHighlight}
         href={'/games/monster-steps/'}
+        githubUrl="https://github.com/gamedevpl/www.gamedev.pl/tree/master/games/monster-steps"
         inProgress={false}
       />
       <GameBox
@@ -38,18 +41,29 @@ export default function OurGames() {
         description={t('Offline multiplayer strategy game')}
         highlight={masterplanHighlight}
         href={'/games/masterplan/'}
+        githubUrl="https://github.com/gamedevpl/www.gamedev.pl/tree/master/games/masterplan"
         inProgress={true}
       />
     </Section>
   );
 }
 
-function GameBox(props: { title: string; description: string; highlight: string; href: string; inProgress: boolean }) {
+function GameBox(props: {
+  title: string;
+  description: string;
+  highlight: string;
+  href: string;
+  githubUrl: string;
+  inProgress: boolean;
+}) {
   return (
     <GameBoxContent href={props.href}>
       <img src={props.highlight} alt={props.title} />
       <h4>{props.title}</h4>
       {props.inProgress && <WorkInProgressBadge>Work in progress</WorkInProgressBadge>}
+      <SourceCodeLink href={props.githubUrl} onClick={(e) => e.stopPropagation()} target="_blank">
+        <SourceCodeIcon>&lt;/&gt;</SourceCodeIcon>
+      </SourceCodeLink>
     </GameBoxContent>
   );
 }
@@ -87,4 +101,29 @@ const WorkInProgressBadge = styled.div`
   transform: rotate(45deg);
   text-align: center;
   z-index: 1;
+`;
+
+const SourceCodeLink = styled.a`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  z-index: 2;
+  text-decoration: none;
+
+  &:hover {
+    > div {
+      background: rgba(0, 0, 0, 0.8);
+    }
+  }
+`;
+
+const SourceCodeIcon = styled.div`
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 4px;
+  padding: 4px 8px;
+  font-family: monospace;
+  font-size: 14px;
+  font-weight: bold;
+  transition: background-color 0.2s;
 `;
