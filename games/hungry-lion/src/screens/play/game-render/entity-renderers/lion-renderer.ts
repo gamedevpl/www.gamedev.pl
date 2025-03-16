b85b3f149b0cdc22eb5f130ca50f6604e6be2658
Grzegorz Tańczyk <fb@tanczyk.pl>
2025-03-16 12:06:33 +0100

    Refactor lion rendering logic to utilize new Lion2d asset rendering; simplify stance determination

--- games/hungry-lion/src/screens/play/game-render/entity-renderers/lion-renderer.ts
+++ games/hungry-lion/src/screens/play/game-render/entity-renderers/lion-renderer.ts
@@ -2,6 +2,8 @@ import { LionEntity } from '../../game-world/entities/entities-types';
 import { LION_WIDTH } from '../../game-world/game-world-consts';
 import { vectorLength } from '../../game-world/utils/math-utils';
 
+import {Lion2d} from '../../../../../../../tools/asset-generator/generator-assets/src/lion-2d/lion-2d'
+
 export function drawLion(ctx: CanvasRenderingContext2D, lion: LionEntity) {
   const width = LION_WIDTH;
   const height = LION_WIDTH;
@@ -9,26 +11,35 @@ export function drawLion(ctx: CanvasRenderingContext2D, lion: LionEntity) {
   const position = lion.position;
   const isMoving = vectorLength(lion.velocity) > 0;
 
-  ctx.save();
-  ctx.translate(position.x, position.y);
-  ctx.rotate(lion.direction);
+  let stance;
+  if (isMoving) {
+    stance = 'walking';
+  } else{
+    stance = 'standing';
+  }    
 
-  // Draw the lion's body (triangle)
-  ctx.beginPath();
-  ctx.moveTo(width / 2, 0);
-  ctx.lineTo(-width / 2, -height / 2);
-  ctx.lineTo(-width / 2, height / 2);
-  ctx.closePath();
+  Lion2d.render(ctx, position.x, position.y, width, height, Date.now() % 1000 / 1000, stance, lion.velocity.x < 0 ? 'left' : 'right');
 
-  const hungerPercentage = lion.hungerLevel / 100;
-  const intensity = isMoving ? '255' : '200';
-  ctx.fillStyle = `rgba(${intensity}, ${intensity}, 0, ${hungerPercentage})`;
-  ctx.fill();
+  // ctx.save();
+  // ctx.translate(position.x, position.y);
+  // ctx.rotate(lion.direction);
 
-  // Add border
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 2;
-  ctx.stroke();
+  // // Draw the lion's body (triangle)
+  // ctx.beginPath();
+  // ctx.moveTo(width / 2, 0);
+  // ctx.lineTo(-width / 2, -height / 2);
+  // ctx.lineTo(-width / 2, height / 2);
+  // ctx.closePath();
 
-  ctx.restore();
+  // const hungerPercentage = lion.hungerLevel / 100;
+  // const intensity = isMoving ? '255' : '200';
+  // ctx.fillStyle = `rgba(${intensity}, ${intensity}, 0, ${hungerPercentage})`;
+  // ctx.fill();
+
+  // // Add border
+  // ctx.strokeStyle = 'black';
+  // ctx.lineWidth = 2;
+  // ctx.stroke();
+
+  // ctx.restore();
 }
