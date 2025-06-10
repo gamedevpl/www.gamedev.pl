c3519b2bd9366bf733f67b99175225938d4a30ab
Grzegorz Tańczyk <fb@tanczyk.pl>
2025-06-10 02:39:38 +0200

    feat: Enhance intro screen layout with styled components for better UI

--- games/tribe/src/components/intro-screen.tsx
+++ games/tribe/src/components/intro-screen.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useGameContext } from '../context/game-context';
+
+const IntroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #000;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-family: 'Press Start 2P', cursive;
+  font-size: 4rem;
+  color: #fff;
+  text-shadow: 4px 4px 0px #ff0000;
+  margin-bottom: 2rem;
+`;
+
+const StartButton = styled.button`
+  font-family: 'Press Start 2P', cursive;
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+  color: #fff;
+  background-color: #ff0000;
+  border: 2px solid #fff;
+  box-shadow: 4px 4px 0px #fff;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #fff;
+    color: #ff0000;
+    box-shadow: 4px 4px 0px #ff0000;
+  }
+`;
 
 export const IntroScreen: React.FC = () => {
   const { setAppState } = useGameContext();
 
   return (
-    <div>
-      <h1>Welcome to Tribe!</h1>
-      <button onClick={() => setAppState('game')}>Start Game</button>
-    </div>
+    <IntroContainer>
+      <Title>Welcome to Tribe!</Title>
+      <StartButton onClick={() => setAppState('game')}>Start Game</StartButton>
+    </IntroContainer>
   );
 };
