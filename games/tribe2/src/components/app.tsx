09cb9d7b64db35784f78f8cbaa4393b5b3894280
Grzegorz Tańczyk <fb@tanczyk.pl>
2025-10-09 09:36:40 +0200

    feat: Enhance Tribe 2 UI with styled components and hover effects

--- games/tribe2/src/components/app.tsx
+++ games/tribe2/src/components/app.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGameContext } from '../context/game-context';
 import { GameScreen } from './game-screen';
+import { BACKGROUND_COLOR } from '../game/game-consts';
 
 // A simple inline style for the intro container.
 const introContainerStyle: React.CSSProperties = {
@@ -9,27 +10,52 @@ const introContainerStyle: React.CSSProperties = {
   alignItems: 'center',
   justifyContent: 'center',
   height: '100vh',
-  backgroundColor: '#1a202c', // A dark background
+  backgroundColor: BACKGROUND_COLOR,
   color: 'white',
-  fontFamily: 'sans-serif',
+};
+
+const titleStyle: React.CSSProperties = {
+  fontFamily: "'Press Start 2P', cursive",
+  fontSize: '4rem',
+  color: 'white',
+  textShadow: '2px 2px 0px red, -2px -2px 0px red, 2px -2px 0px red, -2px 2px 0px red',
+  marginBottom: '2rem',
 };
 
 const buttonStyle: React.CSSProperties = {
+  backgroundColor: 'red',
+  color: 'white',
+  border: '3px solid white',
+  fontFamily: "'Press Start 2P', cursive",
   fontSize: '1.5rem',
   padding: '1rem 2rem',
   cursor: 'pointer',
+  borderRadius: 0,
+  boxShadow: '5px 5px 0px #8B0000',
+  transition: 'all 0.1s ease-in-out',
+};
+
+const buttonHoverStyle: React.CSSProperties = {
+  transform: 'translate(5px, 5px)',
+  boxShadow: '0px 0px 0px #8B0000',
 };
 
 export const App: React.FC = () => {
   const { appState, setAppState } = useGameContext();
+  const [isHovered, setIsHovered] = useState(false);
 
   const renderContent = () => {
     switch (appState) {
       case 'intro':
         return (
           <div style={introContainerStyle}>
-            <h1>Tribe 2</h1>
-            <button style={buttonStyle} onClick={() => setAppState('game')}>
+            <h1 style={titleStyle}>Tribe2</h1>
+            <button
+              style={{ ...buttonStyle, ...(isHovered ? buttonHoverStyle : {}) }}
+              onClick={() => setAppState('game')}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
               Start Game
             </button>
           </div>
@@ -40,8 +66,13 @@ export const App: React.FC = () => {
       default:
         return (
           <div style={introContainerStyle}>
-            <h1>Tribe 2</h1>
-            <button style={buttonStyle} onClick={() => setAppState('game')}>
+            <h1 style={titleStyle}>Tribe2</h1>
+            <button
+              style={{ ...buttonStyle, ...(isHovered ? buttonHoverStyle : {}) }}
+              onClick={() => setAppState('game')}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
               Start Game
             </button>
           </div>
